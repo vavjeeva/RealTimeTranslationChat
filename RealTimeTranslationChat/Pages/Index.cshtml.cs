05f8a598668e80c69295d830f7544b753c7eb584
jeeva.subburaj <vavjeeva@gmail.com>
2018-06-03 16:11:19 -0400

    Add project files.

---
+++ RealTimeTranslationChat/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RealTimeTranslationChat.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Name { get; set; }
+
+        public string Language { get; set; }
+
+        public SelectList LanguageSL { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("./Chat");
+        }
+    }
+}
